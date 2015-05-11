2808eec2a3677f5bb14b5cd1f2b7dae708e1a68b
Lindong <Lindong@Lindongs-MacBook-Pro.local>
2015-05-11 16:42:13 -0700

    Leetcode Day1 #1,2,3 finished

---
+++ #2 Add Two Numbers/#2 LLD.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+/*
+https://leetcode.com/problems/add-two-numbers/
+
+You are given two linked lists representing two non-negative numbers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4) Output: 7 -> 0 -> 8
+*/
+
+class Solution
+{
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+	{
+		int carryon = 0;
+		ListNode *now = new ListNode(0);
+		ListNode *result = now;
+		while(true)
+		{
+			bool looping = (carryon != 0);	// break when carryon == 0 && l1 == NULL && l2 == NULL
+			if(l1 != NULL)
+			{
+				looping = true;
+				carryon += l1->val;
+				l1 = l1->next;
+			}
+			if(l2 != NULL)
+			{
+				looping = true;
+				carryon += l2->val;
+				l2 = l2->next;
+			}
+			if(!looping) break;
+			now->next = new ListNode(carryon % 10);
+			carryon = carryon / 10;
+			now = now->next;
+		}
+		return (result->next != NULL) ? result->next : result;	// take care double NULL
+	}
+};
+
+/*
+idea: Attention to while terminating criteria
+complexity: O(N)
+*/
+
+void Print(ListNode *l)
+{
+	cout << l->val;
+	if(l->next != NULL)
+	{
+		cout << '\t';
+		Print(l->next);
+	}
+	else cout << '\n';
+}
+int main(void)
+{
+	ListNode *l1 = NULL;
+	ListNode *l2 = NULL;
+	Solution engine;
+	Print(engine.addTwoNumbers(l1, l2));
+	return 0;
+}
---
+++ #3 Longest Substring Without Repeating Characters/#3 LLD.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+/*
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+Given a string, find the length of the longest substring without repeating characters.
+For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+For "bbbbb" the longest substring is "b", with the length of 1.
+*/
+
+class Solution
+{
+public:
+	#define FORWARD
+	#ifndef FORWARD
+	int lengthOfLongestSubstring(string s)	// reverse order
+	{
+        int laterPosition[256];	// record char later position in s
+        for(int i = 255; i >= 0; i--)
+        {
+        	laterPosition[i] = s.length();
+        }
+        int *unrepeatlen = new int[s.length()];
+        for(int i = s.length() - 1; i >= 0; i--)
+        {
+        	unrepeatlen[i] = laterPosition[(unsigned int)s[i]] - i;
+        	laterPosition[(unsigned int)s[i]] = i;
+        }
+        int result = 0;
+        for(int i = s.length() - 1; i >= 0; i--)
+        {
+        	int nowlen = unrepeatlen[i];
+        	for(int j = 1; j < nowlen; j++)
+        	{
+        		if(nowlen > (unrepeatlen[i + j] + j))
+        		{
+        			nowlen = unrepeatlen[i + j] + j;
+        		}
+        	}
+        	result = (nowlen > result) ? nowlen : result;
+        }
+        delete []unrepeatlen;
+        return result;
+    }
+    #else
+    int lengthOfLongestSubstring(string s)	// forward order, faster
+    {
+    	int prePosition[256];	// record char previous position in s
+    	for(int i = 255; i >= 0; i--)
+    	{
+    		prePosition[i] = -1;
+    	}
+    	int nowlen = 0, result = 0;
+    	for(int i = 0; i < s.length(); i++)
+    	{
+    		nowlen++;
+    		int unrepeatlen = i - prePosition[(unsigned int)s[i]];
+    		prePosition[(unsigned int)s[i]] = i;
+    		if(nowlen > unrepeatlen)	// limit to char s[i]
+    		{
+    			nowlen = unrepeatlen;
+    		}
+    		result = (nowlen > result) ? nowlen : result;
+    	}
+    	return result;
+    }
+    #endif
+};
+
+/*
+idea: unrepeatlen only consider same char, double for take care other chars between two same char
+complexity: reverse O(N * result), forward O(N)
+*/
+
+int main(void)
+{
+	string s = "abcabcbb";
+	Solution engine;
+	cout << engine.lengthOfLongestSubstring(s) << '\n';
+	return 0;
+}
